5c7f19344f79cb150561a3d9ba077442d14e626d
Diego Ferreira <diegoferreira_devweb@hotmail.com>
2025-02-11 16:34:49 -0300

    Stream Test todo restante

--- src/academy/devdojo/maratonajava/javacore/WEstreams/dominio/LightNovel.java
+++ src/academy/devdojo/maratonajava/javacore/WEstreams/dominio/LightNovel.java
@@ -3,17 +3,25 @@ package academy.devdojo.maratonajava.javacore.WEstreams.dominio;
 public class LightNovel {
     private String title;
     private double price;    
+    private Category category;    
 
 
     @Override
     public String toString() {
-        return "LightNovel [title=" + title + ", price=" + price + "]";
+        return "LightNovel [title=" + title + ", price=" + price + ", category=" + category + "]";
     }
 
 
     public LightNovel(String title, double price) {
         this.title = title;
         this.price = price;
+    }   
+
+
+    public LightNovel(String title, double price, Category category) {
+        this.title = title;
+        this.price = price;
+        this.category = category;
     }
 
 
@@ -24,8 +32,12 @@ public class LightNovel {
 
     public double getPrice() {
         return price;
-    }        
-    
+    }
+
+
+    public Category getCategory() {
+        return category;
+    }          
     
 
 }
